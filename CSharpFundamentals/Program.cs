20f0bcd683192445372848de73cf720ea9fce70a
Tak Bdr. Shrestha <tbshrestha2016@gmail.com>
2022-07-28 10:09:55 +0545

    Class Work-2

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -6,9 +6,16 @@
 //var len = fullName.IndexOf(" ");
 //var firstName = fullName.Substring(0,len);
 //Console.WriteLine($"Hello, {firstName}");
-
+/*
 // Class Work-1
 Console.Write("Enter Your a word: ");
 string word = Console.ReadLine();
 string uword = word.ToUpper();
 Console.WriteLine($"Given word in upper case:, {uword}");
+*/
+// Class Work-2
+Console.Write("Enter meter to convert ot feet");
+var length = Console.ReadLine();
+var lenNumber = double.Parse(length);
+var feets = lenNumber * 3.2808;
+Console.WriteLine($"{lenNumber} metres = {feets} feet");
