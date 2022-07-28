75f05b5e806332d99e7ec3ab459b683bb7151087
Tak Bdr. Shrestha <tbshrestha2016@gmail.com>
2022-07-28 09:51:21 +0545

    Class Work-1

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -1,4 +1,14 @@
 ﻿using System;
-Console.WriteLine("Enter Your Name: ");
-string name = Console.ReadLine();
-Console.WriteLine($"Hello, {name}");
+
+//Console.Write("Enter Your Full Name: ");
+//string name = Console.ReadLine();
+//var fullName = Console.ReadLine();
+//var len = fullName.IndexOf(" ");
+//var firstName = fullName.Substring(0,len);
+//Console.WriteLine($"Hello, {firstName}");
+
+// Class Work-1
+Console.Write("Enter Your a word: ");
+string word = Console.ReadLine();
+string uword = word.ToUpper();
+Console.WriteLine($"Given word in upper case:, {uword}");
