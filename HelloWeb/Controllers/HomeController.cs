1499a475020b32a9b65529b3e34d2703b7795e89
stak <tbshrestha2016@gmail.com>
2022-08-24 00:17:31 +0545

    Add Web Template

--- HelloWeb/Controllers/HomeController.cs
+++ HelloWeb/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public String Test()
+    {
+        return "Hello There!!!";
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
