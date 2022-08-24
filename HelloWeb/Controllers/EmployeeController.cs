399f93b66883b471c39bd006c17c5b6f85922567
Tak Bdr. Shrestha <tbshrestha2016@gmail.com>
2022-08-24 11:26:43 +0545

    Employees Operation

---
+++ HelloWeb/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HelloWeb.Models;
+
+namespace HelloWeb.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // employee/list
+        
+        public IActionResult List()
+        {
+            Employee employee1 = new()
+            {
+                FirstName = "Ram",
+                LastName = "Basnet",
+                Designation = "Software Engineer",
+                level = "7",
+                Department = "HRS",
+
+            };
+
+            Employee employee2 = new()
+            {
+                FirstName = "Ram",
+                LastName = "Basnet",
+                Designation = "Software Engineer",
+                level = "7",
+                Department = "HRS",
+
+            };
+            List<Employee> employees = new List<Employee>() { employee1, employee2};
+            return View(employees);
+        }
+    }
+}
---
+++ HelloWeb/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloWeb.Models;
+
+public class Employee
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Designation { get; set; }
+
+    public string level { get; set; }
+
+    public string Department { get; set; }
+
+}
